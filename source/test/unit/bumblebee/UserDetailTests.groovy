72890e1630e546f40f31a35371170f8d141a19d6
Ernie Paschall <erniep888@yahoo.com>
2012-07-14 00:50:58 -0400

    Adding the user detail test.

---
+++ source/test/unit/bumblebee/UserDetailTests.groovy
@@ -0,0 +1,17 @@
+package bumblebee
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(UserDetail)
+class UserDetailTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
